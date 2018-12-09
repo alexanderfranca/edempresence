379539653ccacc62622beedf0313617cbf67295b
alexanderfranca <affedem@gmail.com>
2018-12-09 18:54:19 -0200

    feat: create class EdemPresence and its first method named timestamp.

---
+++ edempresence/edempresence.py
@@ -0,0 +1,14 @@
+import time
+
+class EdemPresence:
+    """
+    Deals with the whole student ID recording.
+    """
+
+    def __init__(self):
+        pass
+
+    def timestamp(self):
+
+        return int(time.time())
+
