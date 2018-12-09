096dee205667beb763ca97fd302f20ac521d7b69
alexanderfranca <affedem@gmail.com>
2018-12-09 19:46:31 -0200

    test: change Equals for Equal.

--- tests/edempresence_test.py
+++ tests/edempresence_test.py
@@ -33,7 +33,7 @@ class test_edempresence(unittest.TestCase):
 
         file_name_size = len(file_name)
 
-        self.assertEquals(file_name_size, 23) 
+        self.assertEqual(file_name_size, 23) 
 
     def test_if_filename_exists(self):
 
@@ -41,6 +41,6 @@ class test_edempresence(unittest.TestCase):
 
         self.edem.create_file(file_name)
 
-        result = self.edem.filename_exists()
+        result = self.edem.filename_exists(file_name)
 
         self.assertTrue(result)
