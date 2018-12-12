7715a6cd322dd8f8fc780bddced4b4f951c3d1cd
alexanderfranca <affedem@gmail.com>
2018-12-11 22:40:18 -0200

    test: format code.

--- tests/edempresence_test.py
+++ tests/edempresence_test.py
@@ -7,28 +7,20 @@ from edempresence.edemexternalfile import EdemExternalFile
 class test_edempresence(unittest.TestCase):
 
     def setUp(self):
-
         card = EdemCard(enrollment=4324)
         external_file = EdemExternalFile(filepath='./record.txt')
-
         self.edem = EdemPresence(card=card, external_file=external_file)
 
     def test_check_timestamp_is_int(self):
-
         timestamp = self.edem.timestamp()
-
         self.assertTrue(type(timestamp) is int)
 
     def test_if_enrollment_number_is_integer(self):
-
         enrollment = self.edem.enrollment
-
         self.assertTrue(type(enrollment) is int)
 
     def test_method_if_enrollment_number_is_integer(self):
-
         result = self.edem.check_enrollment_is_number()
-
         self.assertTrue(result)
 
     def test_filename_has_twenty_and_three_digits(self):
